0144dbc2561f721ab034af8d5f33506d5e9ee7c8
shohagnawaz <snawazdigital@gmail.com>
2024-09-05 15:45:42 +0600

    uncom

--- src/pages/CheckOut/CheckOut.jsx
+++ src/pages/CheckOut/CheckOut.jsx
@@ -1,19 +1,51 @@
+import { useContext } from "react";
 import { useLoaderData } from "react-router-dom";
+import { AuthContext } from '../../providers/AuthProvider';
 
 const CheckOut = () => {
   const service = useLoaderData();
-  const { _id, title, price } = service;
+  const { _id, title, price, img } = service;
+  const {user} = useContext(AuthContext);
+  const handleBookService = event => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const date = form.date.value;
+    const email = user?.email;
+    const booking = {
+      customerName: name,
+      email, date, img,
+      service: title,
+      service_id: _id,
+      price: price
+    };
+    console.log(booking);
+    // send data to the server
+    fetch('http://localhost:5000/bookings', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(booking)    
+    })
+        .then(res => res.json())
+        .then(data => {
+          console.log(data)
+        })
+  }
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
       <h2 className="text-center text-3xl">Book service: {title}</h2>
       <div >
-        <form>
+        <form onSubmit={handleBookService}>
           <div className="form-control">
             <label className="label">
               <span className="label-text">Name</span>
             </label>
             <input
               type="text"
+              defaultValue={user?.displayName}
               name="name"
               className="input input-bordered"
               required
@@ -36,6 +68,7 @@ const CheckOut = () => {
             </label>
             <input
               type="email"
+              defaultValue={user?.email}
               name="email"
               placeholder="email"
               className="input input-bordered"
